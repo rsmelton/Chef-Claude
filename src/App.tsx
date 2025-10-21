1906deeb2044ae2fc40f64944db034ed701cb501
Robert Melton <rsmelton@crimson.ua.edu>
2025-10-21 09:39:10 -0500

    Took out maincontent component as it was no longer needed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header";
 import IngredientForm from "./components/IngredientForm";
-import MainContent from "./components/MainContent";
 
 function App() {
   return (
